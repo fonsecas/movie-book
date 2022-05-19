1c433ab254733f170223cf881eb9b15ad41c8422
gabrielfonsecah <gabrielfonsecah@gmail.com>
2022-05-19 20:33:02 -0300

    - refinement layout movie details

--- app/src/main/java/com/example/moviebook/presentation/view/home/detail/MovieDetailActivity.kt
+++ app/src/main/java/com/example/moviebook/presentation/view/home/detail/MovieDetailActivity.kt
@@ -2,7 +2,9 @@ package com.example.moviebook.presentation.view.home.detail
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.moviebook.R
@@ -11,6 +13,9 @@ import com.example.moviebook.domain.entity.Movie
 import com.framework.desafio.android.presentation.util.base.BaseActivity
 import com.framework.desafio.android.presentation.util.base.BaseViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class MovieDetailActivity : BaseActivity() {
@@ -32,15 +37,36 @@ class MovieDetailActivity : BaseActivity() {
     }
 
     private fun setupUi() {
-        binding.title.text = movie?.title
-        binding.overview.text = movie?.overview
-        Glide
-            .with(binding.root.context)
-            .load("https://image.tmdb.org/t/p/original/${movie?.posterPath}")
-            .thumbnail()
-            .into(binding.banner)
-        binding.backButton.setOnClickListener { finish() }
+        with (binding) {
+            movie.let {
+                title.text = movie?.title
+                overview.text = movie?.overview
+                year.text = getYearDate(movie?.releaseDate)
+                rate.text = movie?.voteAverage.toString()
+
+                Glide
+                    .with(root.context)
+                    .load("https://image.tmdb.org/t/p/original/${movie?.posterPath}")
+                    .thumbnail()
+                    .into(banner)
+                Glide
+                    .with(root.context)
+                    .load("https://image.tmdb.org/t/p//w500/${movie?.backdropPath}")
+                    .thumbnail()
+                    .into(imgBackground)
+            }
+
+            backButton.setOnClickListener { finish() }
+        }
+
     }
+
+
+    private fun getYearDate(date: String?) : String {
+            val list = date?.split("-")
+            return list?.get(0) ?: ""
+        }
+
 
     companion object {
         private const val MOVIE_EXTRA = "MOVIE_EXTRA"
